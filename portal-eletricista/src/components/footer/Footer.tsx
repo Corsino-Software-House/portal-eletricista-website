161f9e687b63a10694ea199f13b15210cc1cfc12
ElinaldoGuedes <elinaldo.fla@gmail.com>
2025-07-24 17:53:04 -0300

    :sparkles: feat. Atualização geral de CSS

--- portal-eletricista/src/components/footer/Footer.tsx
+++ portal-eletricista/src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 export default function Footer() {
   return (
     <>
-    
+
       <footer className="footer">
        <p className="texto">TODOS OS DIREITOS RESERVADOS 2025</p>
       </footer>
