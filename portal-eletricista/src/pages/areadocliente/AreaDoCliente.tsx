f83ca5bf19054c13149e051e87d43fd83da86d05
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-07-25 03:33:45 -0300

    :sparkles: feat: Adiciona menu com cards e histórico de chamados na área do cliente

--- portal-eletricista/src/pages/areadocliente/AreaDoCliente.tsx
+++ portal-eletricista/src/pages/areadocliente/AreaDoCliente.tsx
@@ -6,9 +6,11 @@ export default function AreaDoCliente() {
   return (
     
      <>
+     <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100vh" }}>
       <Header />
       <FormCliente />
       <Footer />
+      </div>
   </>
   );
 }
