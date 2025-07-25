139191ac8e7d0ececd491784873caea066c8789e
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-07-25 14:15:57 -0300

    :sparkles: feat: Adiciona redirecionamento para áreas do cliente e profissional e ajusta layout do cadastro do cliente

--- portal-eletricista/src/pages/cadastrodocliente/CadastroDoCliente.tsx
+++ portal-eletricista/src/pages/cadastrodocliente/CadastroDoCliente.tsx
@@ -6,12 +6,13 @@ import "./styles.css";
 export default function cadastrocliente() {
   return (
     <>
-      
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100vh" }}>
       <Header />
   
         <CadastroCliente />
 
       <Footer />
+      </div>
     </>
   );
 }
